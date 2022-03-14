12122dcc43e9dcf2920ea868081284ff9d22d73c
Vrunda Thakur <vrundathakur2@gmail.com>
2022-03-14 13:15:08 -0400

    Testing  Async DOM  updates

--- force-app/main/default/lwc/unitTest/unitTest.js
+++ force-app/main/default/lwc/unitTest/unitTest.js
@@ -3,4 +3,7 @@ import { sum } from './sum';
   
 export default class UnitTest extends LightningElement {
   @api unitNumber = sum(2,3);
+  handleChange(event) {
+    this.unitNumber = event.target.value;
+  }
 }
